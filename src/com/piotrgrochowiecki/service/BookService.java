6903d8428cf2ff0fe13febd6b754b42fda21943e
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2023-08-19 18:24:54 +0200

    first 5 functional interfaces

--- src/com/piotrgrochowiecki/service/BookService.java
+++ src/com/piotrgrochowiecki/service/BookService.java
@@ -1,4 +1,14 @@
 package com.piotrgrochowiecki.service;
 
+import com.piotrgrochowiecki.entitiy.Book;
+
+import java.util.ConcurrentModificationException;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
 public class BookService {
+
+    Predicate<Book> isPublishedAfter2000 = book -> book.getYearPublished() > 2000;
+
+    Consumer<Book> printTitleAndAuthor = book -> System.out.println("Title: " + book.getTitle() + " " + book.getAuthor());
 }
--- src/com/piotrgrochowiecki/service/PersonService.java
+++ src/com/piotrgrochowiecki/service/PersonService.java
@@ -1,4 +1,30 @@
 package com.piotrgrochowiecki.service;
 
+import com.piotrgrochowiecki.entitiy.Person;
+
+import java.util.Comparator;
+import java.util.Random;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.random.RandomGenerator;
+import java.util.random.RandomGeneratorFactory;
+
 public class PersonService {
+
+    Comparator<Person> PeopleByAgeAscendingComparator = (Person p1, Person p2) -> Integer.compare(p1.getAge(), p2.getAge());
+
+    Function<Person, String> PersonToNameFunction = Person::getName;
+
+    Supplier<Person> randomPersonSupplier = () -> {
+        String[] names = {"Alice", "Bob", "Charlie", "Dave", "Eve"};
+        String[] addresses = {"123 Main St", "456 Elm St", "789 Oak St", "246 Maple St", "135 Pine St"};
+
+        Random random = new Random();
+
+        String name = names[random.nextInt(names.length)];
+        String address = addresses[random.nextInt(addresses.length)];
+        Integer age = random.nextInt(12, 75);
+
+        return new Person(name, age, address);
+    };
 }
