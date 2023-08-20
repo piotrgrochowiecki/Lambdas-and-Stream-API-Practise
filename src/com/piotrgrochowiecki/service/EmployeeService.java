407b60c385638754f17f022b6113af3b4def1384
Piotr Grochowiecki <42697061+piotrgrochowiecki@users.noreply.github.com>
2023-08-20 17:28:17 +0200

    lambda expressions

--- src/com/piotrgrochowiecki/service/EmployeeService.java
+++ src/com/piotrgrochowiecki/service/EmployeeService.java
@@ -1,4 +1,36 @@
 package com.piotrgrochowiecki.service;
 
+import com.piotrgrochowiecki.entitiy.Employee;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.*;
+import java.util.stream.Collectors;
+
 public class EmployeeService {
+
+    Function<List<Employee>, Map<String, List<Employee>>> sortEmployeeByDepartmentFunction = employees -> employees.stream()
+            .collect(Collectors.groupingBy(Employee::getDepartment));
+
+    Function<List<Employee>, Double> calculateAverageSalaryFunction = employees -> employees.stream()
+            .mapToDouble(Employee::getSalary)
+            .average()
+            .orElse(0.0);
+
+
+    Function<List<Employee>, Map<String, Double>> getMapOfNamesAndSalariesFunction = employees -> employees.stream()
+            .collect(Collectors.toMap(Employee::getName, Employee::getSalary));
+
+    Function<List<Employee>, String> getMostCommonSkillFunction = employees -> employees.stream()
+            .flatMap(employee -> employee.getSkills().stream())
+            .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+            .entrySet()
+            .stream()
+            .max(Map.Entry.comparingByValue())
+            .map(Map.Entry::getKey)
+            .orElse(null);
+
+    BiFunction<Employee, Employee, List<String>> findCommonSkillsFunction = (e1 ,e2) -> e1.getSkills().stream()
+            .filter(e2.getSkills()::contains)
+            .collect(Collectors.toList());
 }
--- src/com/piotrgrochowiecki/service/MovieService.java
+++ src/com/piotrgrochowiecki/service/MovieService.java
@@ -1,5 +1,32 @@
 package com.piotrgrochowiecki.service;
 
+import com.piotrgrochowiecki.entitiy.Movie;
+
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 public class MovieService {
 
+    Predicate<Movie> isRatingAbove8p0 = m -> m.getRating() > 8.0;
+
+    BiFunction<Movie, Movie, List<String>> findCommonGenresFunction = (m1, m2) -> {
+        return m1.getGenres().stream()
+                .filter(m2.getGenres()::contains)
+                .toList();
+    };
+
+    Function<List<Movie>, Map<String, List<Movie>>> groupByDirectorFunction = movies -> movies.stream()
+            .collect(Collectors.groupingBy(Movie::getDirector));
+
+    Function<List<Movie>, Double> getAverageRatingFunction = movies -> movies.stream()
+            .mapToDouble(Movie::getRating)
+            .average()
+            .orElse(0.0);
+
+    Function<List<Movie>, Map<String, Double>> getMapOfTitlesAndRatings = movies -> movies.stream()
+            .collect(Collectors.toMap(Movie::getTitle, Movie::getRating));
 }
